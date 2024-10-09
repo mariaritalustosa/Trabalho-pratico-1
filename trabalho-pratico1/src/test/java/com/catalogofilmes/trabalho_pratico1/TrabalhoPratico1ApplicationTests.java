e2e7bd30f73d92d1ff82a8e9b687d77bd6a190f0
Maria Rita Lustosa <mritalustosa.ad@gmail.com>
2024-10-08 21:49:45 -0300

    commit

---
+++ trabalho-pratico1/src/test/java/com/catalogofilmes/trabalho_pratico1/TrabalhoPratico1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalogofilmes.trabalho_pratico1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoPratico1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
